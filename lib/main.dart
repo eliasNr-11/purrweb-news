2e164e6110a8e4f440a9bdeb4e7a62d0aa0c80ec
eliasNr-11 <eliasnurhussien43@gmail.com>
2024-01-25 20:36:19 +0300

    setup packages and folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pureweb_news/presentation/routes.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp.router(
+          routerConfig: routes(),
+        );
+      }
     );
   }
 }
---
+++ lib/presentation/routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pureweb_news/presentation/screens/home_page.dart';
+
+GoRouter routes() {
+  return GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomePage();
+        },
+        routes: const <RouteBase>[
+          // GoRoute(
+          //   path: 'details',
+          //   builder: (BuildContext context, GoRouterState state) {
+          //     return const DetailsScreen();
+          //   },
+          // ),
+        ],
+      ),
+    ],
+  );
+}
